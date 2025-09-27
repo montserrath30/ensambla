afdb687f407c08cae00b006f3edae26d8e2b5479
montserrath30 <montserrath.jaen@ucr.ac.cr>
2025-09-26 19:22:40 -0600

    advance

--- src/calculadora.cpp
+++ src/calculadora.cpp
@@ -1,6 +1,27 @@
-void class Calculadora {
-public:
-    int sumar(int a, int b) {
-        return a + b;
+#include <iostream>
+#include <bitset>
+#include <string>
+#include <sstream>
+#include <iomanip>
+
+using namespace std;
+
+
+void operaciones(int num1, int num2, string operacion) {
+    if (operacion == "suma") {
+        cout << "Resultado de la suma: " << num1 + num2 << endl;
+    } else if (operacion == "resta") {
+        cout << "Resultado de la resta: " << num1 - num2 << endl;
+    } else if (operacion == "multiplicacion") {
+        cout << "Resultado de la multiplicacion: " << num1 * num2 << endl;
+    } else if (operacion == "division") {
+        if (num2 != 0) {
+            cout << "Resultado de la division: " << static_cast<double>(num1) / num2 << endl;
+        } else {
+            cout << "Error: Division por cero." << endl;
+        }
+    } else {
+        cout << "Operacion no reconocida." << endl;
     }
-};  
+}
+
---
+++ src/calculadora.hpp
@@ -0,0 +1,14 @@
+#ifndef CALCULADORA_HPP
+#define CALCULADORA_HPP
+#include <string>
+
+using namespace std;
+
+
+void operaciones(int num1, int num2, string operacion);
+
+void convertitBase(int numero);
+
+void complementos(int numero);
+
+#endif // CALCULADORA_HPP
